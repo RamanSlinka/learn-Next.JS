3d9bb173159ae2b202b75f49e16ae41d36e93360
RamanSlinka <romuch1986@gmail.com>
2022-01-11 22:58:55 +0100

    Fetching Data

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,36 @@
 import Heading from "../../Components/Heading";
 import Head from "next/head";
 
-const Contacts = () => {
+
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    if(!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {contacts: data},
+    }
+};
+
+
+const Contacts = ({contacts}) => {
+
     return (
         <>
             <Head>
                 <title>Contacts</title>
             </Head>
             <Heading text={"Contacts list:"}/>
+            <ul>
+                {contacts && contacts.map(({id, name, email}) => (
+                    <li key={id}><strong>{name}</strong>({email})</li>
+                ))}
+            </ul>
         </>
 
     );
